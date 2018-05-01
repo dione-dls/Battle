0bf707a40c2c9dcd021b8678727c5a3a9c570edb
Charlene <charlenechetcuti@gmail.com>
2018-05-01 17:14:20 +0100

    added player 2 hit points

--- spec/features/entering_players_spec.rb
+++ spec/features/entering_players_spec.rb
@@ -4,7 +4,16 @@ RSpec.feature "Entering players" do
     fill_in('player1', :with => 'Charlene')
     fill_in('player2', :with => 'Dione')
     click_button 'Submit'
-    expect(page).to have_text("Player 1's name is Charlene")
-    expect(page).to have_text("Player 2's name is Dione")
+    names = ("Player 1's name is Charlene\nPlayer 2's name is Dione")
+    expect(page).to have_text names
+  end
+end
+
+RSpec.feature "Hit points" do
+  scenario "viewing opponent's hit points" do
+    visit "/play"
+    expect(find('progress')['value']).to eq('40')
+    expect(find('progress')['max']).to eq('100')
+    expect(page).to have_text("Player 2's hit points is 40")
   end
 end
