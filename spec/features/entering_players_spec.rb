bfd51c7b3ef848dd5b7a2370abdbc5dff2ea2caf
Dione Estabillo <dione.estabillo@gmail.com>
2018-05-02 12:23:35 +0100

    Refactored feature test with sign_in_and_play method

--- spec/features/entering_players_spec.rb
+++ spec/features/entering_players_spec.rb
@@ -1,9 +1,6 @@
 RSpec.feature "Entering players" do
   scenario "Each can enter their names" do
-    visit "/"
-    fill_in('player1', :with => 'Charlene')
-    fill_in('player2', :with => 'Dione')
-    click_button 'Submit'
+    sign_in_and_play
     names = ("Player 1's name is Charlene\nPlayer 2's name is Dione")
     expect(page).to have_text names
   end
@@ -11,7 +8,7 @@ end
 
 RSpec.feature "Hit points" do
   scenario "viewing opponent's hit points" do
-    visit "/play"
+    sign_in_and_play
     expect(find('progress')['value']).to eq('40')
     expect(find('progress')['max']).to eq('100')
     expect(page).to have_text("Player 2's hit points is 40")
