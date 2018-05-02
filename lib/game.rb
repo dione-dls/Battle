972adf7b6f27f8bd9ee91b8b7fec0c077ac12cf4
Dione Estabillo <dione.estabillo@gmail.com>
2018-05-02 16:48:23 +0100

    Adding arguments to game

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,9 @@
 class Game
+
+  def initialize(player1, player2)
+
+  end
+
   def attack(player)
     player.reduce_health
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,15 @@
 require_relative '../lib/game'
 
-describe Game do
+describe Game, :game do
+
+  subject { Game.new(player, player) }
   let(:player) { instance_double Player, reduce_health: nil }
+
+  describe '#initialize' do
+    it 'takes two arguments when created' do
+      expect(Game).to respond_to(:new).with(2).arguments
+    end
+  end
 
   describe '#attack' do
     it 'reduces opponents health' do
